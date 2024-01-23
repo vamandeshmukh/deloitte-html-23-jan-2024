46e89654439762b4c02c6d0ee08b8c9b7dd96e49
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-01-23 14:43:19 +0530

    html

--- 033-com.js
+++
@@ -1,7 +0,0 @@
-
-const { Network, cidrSubnet } = require('ip');
-const network = new Network('192.168.0.0/24');
-const subnets = network.subnets(1);
-subnets.forEach(subnet => {
-    console.log(subnet);
-});
